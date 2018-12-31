e6564e8706c75f3b19ebd4ec6b5b30d400fa5277
udith victor <udithv01@gmail.com>
2018-12-31 22:30:51 +0530

    added prism js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,23 @@ module.exports = {
                 path: `${__dirname}/src`
             }
         },
-        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            classPrefix: "language-",
+                            inlineCodeMarker: null,
+                            aliases: {},
+                            showLineNumbers: false,
+                            noInlineHighlight: false,
+                        }
+                    }
+                ]
+            }
+        },
         `gatsby-plugin-emotion`,
         {
             resolve: `gatsby-plugin-typography`,
